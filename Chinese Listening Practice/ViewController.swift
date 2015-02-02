5fa7083dd64849cb2d2f3563b7a94f3c247827e5
Akira Nakagawa <anakaga@linfield.edu>
2015-02-02 14:02:05 -0800

    Initial Commit

---
+++ Chinese Listening Practice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chinese Listening Practice
+//
+//  Created by 中川 亮 on 2015/02/02.
+//  Copyright (c) 2015年 中川 亮. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
